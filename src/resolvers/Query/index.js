ddecbcf750cfd0836260df4ea06934446ac90505
James A. Joy <joyja@jarautomation.io>
2020-11-11 07:16:48 +0000

    start of proles

--- src/resolvers/Query/index.js
+++ src/resolvers/Query/index.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch')
 
-const containers = async function (root, args, context, info) {
-  const result = await fetch(`${context.lxdEndpoint}/1.0/instances`, {
-    agent: context.agent,
+const containers = async function (root, args, { lxdEndpoint, agent }, info) {
+  const result = await fetch(`${lxdEndpoint}/1.0/instances`, {
+    agent,
   })
   const { metadata: containers } = await result.json()
   const requestPromises = containers.map((container) => {
-    return fetch(`${context.lxdEndpoint}${container}`, {
-      agent: context.agent,
+    return fetch(`${lxdEndpoint}${container}`, {
+      agent,
     })
   })
   const containerResponses = await Promise.all(requestPromises)
@@ -16,7 +16,26 @@ const containers = async function (root, args, context, info) {
   return containerData.map((d) => d.metadata)
 }
 
+const profiles = async function (root, args, { lxdEndpoint, agent }, info) {
+  return fetch(`${lxdEndpoint}/1.0/profiles`, {
+    agent,
+  }).then((result) => {
+    return result.json().then((data) => {
+      return Promise.all(
+        data.metadata.map((profile) => {
+          return fetch(`${lxdEndpoint}${profile}`, {
+            agent,
+          }).then((result) => {
+            return result.json().then((data) => data.metadata)
+          })
+        })
+      )
+    })
+  })
+}
+
 module.exports = {
   info: () => `IIOT application container manger.`,
   containers,
+  profiles,
 }
