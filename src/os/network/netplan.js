008af73e7cbc79fe086d3cc97fabe5c5cb6a0b29
James A. Joy <joyja@jarautomation.io>
2021-01-22 20:22:56 +0000

    added network utils

--- src/os/network/netplan.js
+++ src/os/network/netplan.js
@@ -1,74 +1,65 @@
+const fs = require('fs')
+const yaml = require('js-yaml')
+const isCidr = require('is-cidr')
+const isIp = require('is-ip')
 const { exec } = require('child_process')
 
-const getInterfaces = async function () {
-  return new Promise((resolve, reject) => {
-    exec('ip addr show', (err, stdout, stderr) => {
-      const eth1identifier = `${process.env.ETH1NAME}`
-      const eth2identifier = `${process.env.ETH2NAME}`
-      const interfaces = stdout
-        .split('\n') //split into members by newline
-        .filter(String) //Only get valid strings
-        .filter((line) => !line.startsWith(' ')) //Filter out the detail lines (they have leading spaces)
-        .filter((line) => !line.includes('lo:')) //Filter out the loopback interface
-        .filter(
-          (line) =>
-            line.includes(eth1identifier) || line.includes(eth2identifier)
-        ) //Keep only the interfaces with the names set in the environment variables
-        .map((line) => {
-          const raw = line.trim()
-          const fields = raw.split(/\s+/)
-          let alias = fields[1].split('@')[0].replace(':', '')
-          if (line.includes(eth1identifier)) {
-            alias = process.env.ETH1ALIAS
-          } else if (line.includes(eth2identifier)) {
-            alias = process.env.ETH2ALIAS
-          }
-          return {
-            id: fields[0].replace(':', ''),
-            name: fields[1].split('@')[0].replace(':', ''),
-            alias,
-            mtu: fields[4],
-            state: fields[8],
-          }
-        })
-      interfaces.forEach((interface, index) => {
-        const ipAddresses = []
-        let lineReached = false
-        stdout
-          .split('\n')
-          .filter(String)
-          .forEach((line) => {
-            if (
-              !lineReached &&
-              !line.startsWith(' ') &&
-              line.includes(interface.name)
-            ) {
-              lineReached = true
-            } else if (
-              lineReached &&
-              line.startsWith('') &&
-              line.includes('inet ')
-            ) {
-              const fields = line.trim().split(/\s+/)
-              ipAddresses.push(fields[1])
-            } else if (
-              lineReached &&
-              line.startsWith('') &&
-              line.includes('link/ether')
-            ) {
-              const fields = line.trim().split(/\s+/)
-              interfaces[index].macAddress = fields[1]
-            } else if (lineReached && !line.startsWith(' ')) {
-              lineReached = false
-            }
-          })
-        interfaces[index].ipAddresses = ipAddresses
-      })
-      resolve(interfaces)
-    })
+const getConfig = function () {
+  const dirPath = '/etc/netplan/'
+  return fs.readdirSync(dirPath).map((filePath) => {
+    fileContents = fs.readFileSync(dirPath + filePath)
+    return {
+      path: filePath,
+      contents: yaml.safeLoad(fileContents),
+    }
   })
 }
 
+const setInterfaceConfig = function (updates) {
+  if (updates) {
+    const config = getConfig()[0]
+    if (config.contents.network.ethernets[updates.name]) {
+      const newConfig = {}
+      newConfig.dhcp4 = !!updates.dhcp4
+      if (!updates.dhcp4) {
+        if (updates.addresses) {
+          updates.addresses.forEach((address, index) => {
+            if (!isCidr.v4(address)) {
+              throw Error(
+                `IP address with CIDR at index ${index}: ${address} is not valid.`
+              )
+            }
+          })
+          newConfig.addresses = updates.addresses
+        }
+        if (updates.gateway4) {
+          if (isIp.v4(updates.gateway4)) {
+            newConfig.gateway4 = updates.gateway4
+          } else {
+            throw Error(`Gateway must be a valid IPv4 address.`)
+          }
+        }
+      }
+      config.contents.network.ethernets[updates.name] = newConfig
+      const yamlDump = yaml.safeDump(config.contents)
+      fs.writeFileSync('/etc/netplan/' + config.path, yamlDump)
+      exec('sudo netplan apply', (error, stdout, stderr) => {
+        if (error) console.error(error)
+        if (stderr) console.error(stderr)
+      })
+    } else {
+      if (updates.name) {
+        throw new Error(`Interface wth name ${updates.name} does not exist.`)
+      } else {
+        throw new Error(`Interface name must be specified.`)
+      }
+    }
+  } else {
+    throw new Error('Interface updates must be defined.')
+  }
+}
+
 module.exports = {
-  getInterfaces,
+  getConfig,
+  setInterfaceConfig,
 }
