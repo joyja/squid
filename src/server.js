9667892e3be604bdc02fa0568f224d7d3e67194f
James A. Joy <joyja@jarautomation.io>
2021-01-28 16:09:58 +0000

    again

--- src/server.js
+++ src/server.js
@@ -7,8 +7,6 @@ const express = require('express')
 const { ApolloServer, PubSub, gql } = require('apollo-server-express')
 const resolvers = require('./resolvers')
 
-app.use(express.json())
-
 const agent = new https.Agent({
   cert: fs.readFileSync(path.resolve('./certificates/lxd.crt'), 'utf-8'),
   key: fs.readFileSync(path.resolve('./certificates/lxd.key'), 'utf-8'),
@@ -20,6 +18,8 @@ const lxdPort = process.env.FACTOTUM_LXD_PORT || 8443
 const lxdEndpoint = `https://${lxdHost}:${lxdPort}`
 
 const app = express()
+
+app.use(express.json())
 
 app.post('/phone-home', async (req, res, next) => {
   console.log(req)
